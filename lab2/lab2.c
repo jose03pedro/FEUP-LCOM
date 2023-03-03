b5c656d5cc510421d8c29158a8e16a458248ec7b
jose03pedro <up202108673@edu.fe.up.pt>
2023-03-03 15:42:34 +0000

    First week

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,9 +1,8 @@
-#include <lcom/lcf.h>
 #include <lcom/lab2.h>
+#include <lcom/lcf.h>
 
 #include <stdbool.h>
 #include <stdint.h>
-
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -30,22 +29,28 @@ int main(int argc, char *argv[]) {
 }
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (timer < 0 || timer > 2)
+    return 1;
+  uint8_t status;
+  timer_get_conf(timer, &status);
+  timer_display_conf(timer, field, status);
+  return 0;
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (timer < 0 || timer > 2)
+    return 1;
+  timer_set_frequency(timer, freq);
+  return 0;
 }
 
 int(timer_test_int)(uint8_t time) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  uint8_t timer_mask;
+  if (timer_subscribe_int(&timer_mask))
+    return 1;
+  uint8_t interrupt = 60 * time;
+  uint8_t i = 0;
+  while (i <= interrupt) {
+    if ()
+  }
 }
--- lab2/utils.c
+++ lab2/utils.c
@@ -3,20 +3,25 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  if(*LSB == NULL) return 1;
-  *LSB = val & 0xFF;
+  if (lsb == NULL)
+    return 1;
+  *lsb = val & 0xFF;
   return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  if(*MSB == NULL) return 1;
-  *MSB = (val >> 8) & 0xFF;
+  if (msb == NULL)
+    return 1;
+  *msb = (val >> 8) & 0xFF;
   return 0;
 }
 
-int (util_sys_inb)(int port, uint8_t *value) {
- uint32_t value;
-  if(sysinb(port, &value) != 0) return 1;
-  *output = value & 0xFF;
+int(util_sys_inb)(int port, uint8_t *value) {
+  uint32_t valor = *value;
+  if (value == NULL)
+    return 1;
+  if (sys_inb(port, &valor) != OK)
+    return 1;
+  *value = valor & 0xFF;
   return 0;
 }
